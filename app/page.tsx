b5c6e9747a47d200611a4b1d44e411b1bc281129
Theo-Coquio <coquiotheo@gmail.com>
2025-03-17 22:18:12 +0100

    globals css added

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import Hero from "./Components/Hero";
+
 export default function Home() {
-  return <div className="text-5xl text-violet-300 font-bold">Zentry</div>;
+  return (
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero />
+    </main>
+  );
 }
